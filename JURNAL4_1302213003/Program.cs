ffaa8ebc381a2a6943f99d0fdfaa75dccd7cb939
Marsyarayyy <marsyarh09@gmail.com>
2023-03-15 17:18:55 +0700

    menambahkan class PosisiKarakterGame

--- JURNAL4_1302213003/Program.cs
+++ JURNAL4_1302213003/Program.cs
@@ -14,12 +14,19 @@ namespace JURNAL4_1302213003
             a.getallkodebuah();
             Console.WriteLine("\n");
 
+            Console.WriteLine("POSISI AWAL PLAYER :");
+
+            posisikaraktergame posisi = new posisikaraktergame();
+
+            posisi.key();
+
+
         }
     }
     class kodeBuah
     {
         Dictionary<String, string> buah = new Dictionary<String, string>(){
-            {"Apel = ", "A00"},
+            {"Apel", "A00"},
             {"Aprikot", "B00"},
             {"Alpukat", "C00"},
             {"Pisang", "D00"},
@@ -51,4 +58,61 @@ namespace JURNAL4_1302213003
             }
         }
     }
-}
+
+    class posisikaraktergame
+    {
+        enum state { Tengkurap, Jongkok, Berdiri, Terbang };
+
+        public void key()
+        {
+            state posisi = state.Berdiri;
+            
+
+            string[] status_posisi = { "Tengkurap", "Jongkok", "Berdiri", "Terbang" };
+            do
+            {
+                Console.WriteLine("Posisi " + status_posisi[(int)posisi]);
+                Console.WriteLine("Command : ");
+                String command = Console.ReadLine();
+                switch (posisi)
+                {
+                    case state.Berdiri:
+                        if (command == "s")
+                        {
+                            posisi = state.Jongkok;
+
+                        }
+                        else if (command == "w")
+                        {
+                            posisi = state.Terbang;
+
+                        }
+                        break;
+                    case state.Jongkok:
+                        if (command == "s")
+                        {
+                            posisi = state.Tengkurap;
+                            Console.WriteLine("Posisi Istirahat");
+
+                        }
+                        else if (command == "w")
+                        {
+                            posisi = state.Berdiri;
+                            Console.WriteLine("Posisi Standby");
+                        }
+                        break;
+                    case state.Tengkurap:
+                        if (command == "w")
+                        {
+                            posisi = state.Jongkok;
+
+                        }
+                        break;
+
+                    }
+                } while (true);
+            }
+
+        }
+    }
+
